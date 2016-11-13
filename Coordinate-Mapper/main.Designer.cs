d6b0c81b4dd274978c6648be856c32b4686777e8
AFulkerson <adrienne.fulkerson@gmail.com>
2016-11-13 00:48:47 -0500

    update

---
+++ Coordinate-Mapper/main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Coordinate_Mapper
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.btn_selectFile = new System.Windows.Forms.Button();
+            this.btn_runScript = new System.Windows.Forms.Button();
+            this.lbl_selectedFilelbl = new System.Windows.Forms.Label();
+            this.lbl_selectedFile = new System.Windows.Forms.Label();
+            this.tb_results = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_selectFile
+            // 
+            this.btn_selectFile.Location = new System.Drawing.Point(13, 13);
+            this.btn_selectFile.Name = "btn_selectFile";
+            this.btn_selectFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_selectFile.TabIndex = 0;
+            this.btn_selectFile.Text = "Select File";
+            this.btn_selectFile.UseVisualStyleBackColor = true;
+            this.btn_selectFile.Click += new System.EventHandler(this.btn_selectFile_Click);
+            // 
+            // btn_runScript
+            // 
+            this.btn_runScript.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_runScript.Location = new System.Drawing.Point(249, 13);
+            this.btn_runScript.Name = "btn_runScript";
+            this.btn_runScript.Size = new System.Drawing.Size(75, 23);
+            this.btn_runScript.TabIndex = 1;
+            this.btn_runScript.Text = "Run";
+            this.btn_runScript.UseVisualStyleBackColor = true;
+            this.btn_runScript.Click += new System.EventHandler(this.btn_runScript_Click);
+            // 
+            // lbl_selectedFilelbl
+            // 
+            this.lbl_selectedFilelbl.AutoSize = true;
+            this.lbl_selectedFilelbl.Location = new System.Drawing.Point(13, 43);
+            this.lbl_selectedFilelbl.Name = "lbl_selectedFilelbl";
+            this.lbl_selectedFilelbl.Size = new System.Drawing.Size(71, 13);
+            this.lbl_selectedFilelbl.TabIndex = 2;
+            this.lbl_selectedFilelbl.Text = "Selected File:";
+            // 
+            // lbl_selectedFile
+            // 
+            this.lbl_selectedFile.AutoSize = true;
+            this.lbl_selectedFile.Location = new System.Drawing.Point(90, 43);
+            this.lbl_selectedFile.Name = "lbl_selectedFile";
+            this.lbl_selectedFile.Size = new System.Drawing.Size(35, 13);
+            this.lbl_selectedFile.TabIndex = 3;
+            this.lbl_selectedFile.Text = "label2";
+            this.lbl_selectedFile.TextChanged += new System.EventHandler(this.lbl_selectedFile_TextChanged);
+            // 
+            // tb_results
+            // 
+            this.tb_results.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_results.Location = new System.Drawing.Point(13, 60);
+            this.tb_results.Multiline = true;
+            this.tb_results.Name = "tb_results";
+            this.tb_results.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_results.Size = new System.Drawing.Size(311, 239);
+            this.tb_results.TabIndex = 4;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 311);
+            this.Controls.Add(this.tb_results);
+            this.Controls.Add(this.lbl_selectedFile);
+            this.Controls.Add(this.lbl_selectedFilelbl);
+            this.Controls.Add(this.btn_runScript);
+            this.Controls.Add(this.btn_selectFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(352, 350);
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Coordinate Mapper";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_selectFile;
+        private System.Windows.Forms.Button btn_runScript;
+        private System.Windows.Forms.Label lbl_selectedFilelbl;
+        private System.Windows.Forms.Label lbl_selectedFile;
+        private System.Windows.Forms.TextBox tb_results;
+    }
+}
+
---
+++ Coordinate-Mapper/main.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace Coordinate_Mapper
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void displayError(Exception e)
+        {
+            MessageBox.Show("Something went wrong!\n\n" + e.Message + "\n\nI just can't go on...", "Error", MessageBoxButtons.OK);
+        }
+
+        private void appResults(string txt)
+        {
+            tb_results.AppendText(txt + Environment.NewLine);
+        }
+
+        private string getFilePath()
+        {
+            string fp = null;       //SVG FILE PATH
+
+            OpenFileDialog fd = new OpenFileDialog();
+
+            fd.Filter = "SVG (.svg)|*.svg";
+            fd.FilterIndex = 1;
+
+            fd.Multiselect = false;
+
+            // GETS FILE PATH. IF CANCELED, RETURNS THE LAST FILE SELECTED
+            DialogResult dr = fd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                fp = fd.FileName;
+            }
+            else if (dr == DialogResult.Cancel)
+            {
+                fp = globalVars.filePath;
+            }
+
+            fd.Dispose();
+            return fp;
+
+        }
+
+        private void get_coord(string filePath)
+        {
+            //CALCULATE COORDINATES FROM SVG FILE AND WRITE THEM TO THE CSV OUTPUT FILE
+
+            //LOAD SVG FILE
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(filePath);
+            }
+            catch (Exception er)
+            {
+                displayError(er);
+                return;
+            }
+
+            XmlElement el = doc.DocumentElement;
+
+            // CREATE LIST FOR THE OUTPUT - FIRST LINE IS HEADERS
+            // EACH ITEM IN THE LIST IS A COMPLETE LINE FOR THE OUTPUT FILE THAT WILL BE 
+            // WRITTEN TO THE COMMA DELIMMITED CSV FILE. 
+            List<string> outputRecords = new List<string>();
+
+            // LIST OF COORDINATES FOR EACH PATH. IF PATH IS POLY THEN THIS LIST WILL BE JOINED WITH
+            // OUTPUTRECORDS LIST
+            List<string> outputCoord = new List<string>();
+
+            // LIST OF RESULTS FOR EACH POLY
+            List<string> polyResults = new List<string>();
+
+            // GET HEIGHT OF SVG CANVAS. USED TO CALC INVERTED Y COORD
+            int height = 0;
+            int width = 0;
+            //if (el.HasAttribute("viewBox"))
+            try
+            {
+                string[] docSize = el.GetAttribute("viewBox").Split();
+                height = Convert.ToInt32(Math.Round(Convert.ToDecimal(docSize[3])));
+                width = Convert.ToInt32(Math.Round(Convert.ToDecimal(docSize[2])));
+            }
+            catch (Exception er)
+            {
+                displayError(er);
+                return;
+            }
+
+            //VARS FOR GETTING COORDINATES
+            long order = 0;                 //POINT ORDER
+            Int32 pathOrder = 0;            //PATH ORDER (RESETS FOR EACH PATH)
+            decimal x = 0, y = 0;
+            string id, d, resultCP = null;  //PATH ID FROM SVG, PATH COORDINATE STRING FROM SVG, CONFIRMATION MESSAGE POLY MAPPED
+            //string result1 = null;             //CONFIRM POLY MAPPED
+            string[] coordPair;             //ARRAY OF COORDINATE PAIRS WHEN d IS SPLIT
+            string[] pairTemp;              //USED TO STORE THE SPLIT COORDPAIR ([0] = X [1] = Y)
+            bool absPath = false;           //CHECK IF PATH USES ABSOLUTE POSITIONING FOR COORDINATES
+
+            outputRecords.Add("Path Name, Point Order, X, Y");      // FIRST LINE OF OUTPUT FILE
+            //appResults("Results:");
+            //appResults("");
+
+            //GET COORDINATES FOR EACH POLY
+            foreach (XmlElement item in el.GetElementsByTagName("path"))
+            {
+                pathOrder = 0;
+                x = 0;
+                y = 0;
+                id = item.GetAttribute("id");
+                d = item.GetAttribute("d");
+                coordPair = d.Split();
+
+                foreach (string pair in coordPair)
+                {
+                    order++;
+                    pairTemp = pair.Split(',');
+
+                    double Num;
+                    bool isNum = double.TryParse(pairTemp[0], out Num);
+
+                    if (isNum)
+                    {
+                        if (absPath)
+                        {
+                            x = Convert.ToDecimal(pairTemp[0]);
+                            y = height - Convert.ToDecimal(pairTemp[1]);
+                        }
+                        else
+                        {
+                            x = x + Convert.ToDecimal(pairTemp[0]);
+                            if (pathOrder == 0)
+                            {
+                                y = height - Convert.ToDecimal(pairTemp[1]);
+                            }
+                            else
+                            {
+                                y = y - Convert.ToDecimal(pairTemp[1]);
+                            }
+                        }
+
+                        outputCoord.Add(id + "," + order + "," + x + "," + y);
+                        resultCP = "Polygon: " + id + " created successfully";
+                        pathOrder++;
+                    }
+                    else
+                    {
+                        if (pairTemp[0].Equals("M"))
+                        {
+                            absPath = true;
+                            resultCP = "";
+                        }
+                        else if (pairTemp[0].Equals("m"))
+                        {
+                            absPath = false;
+                            resultCP = "";
+                        }
+                        // (ANY DRAW COMMAND THAT ISN'T 'M/m' [DRAW TO] OR 'Z/z' [ENDS PATH] IDENTIFIES THE
+                        // PATH AS A NON-POLYGON SHAPE AND WILL BE SKIPPED
+                        else if (!(pairTemp[0].Equals("Z")) && !(pairTemp[0].Equals("z")))
+                        {
+                            outputCoord.Clear();
+                            resultCP = "Skipped shape: " + id + ". Not a polygon";
+                            break;
+                        }
+                    }
+                }
+                //UPDATE OUTPUT
+                //appResults(resultCP);
+                polyResults.Add(resultCP);
+                outputRecords.AddRange(outputCoord);
+                outputCoord.Clear();
+            }
+
+            string outputFilePath = filePath.Substring(0, filePath.Length - 4) + "_coordinates.csv";
+
+            try
+            {
+                System.IO.File.WriteAllLines(outputFilePath, outputRecords);
+                //FINISH SCRIPT 
+                appResults("Results:");
+                appResults("");
+                foreach (string item in polyResults)
+                {
+                    appResults(item);
+                }
+                appResults("");
+                appResults("Output file: " + outputFilePath + " saved");
+                appResults("");
+                appResults("Image height: " + height);
+                appResults("Image width: " + width);
+                appResults("");
+                appResults("Script complete");
+            }
+            catch (Exception er)
+            {
+                displayError(er);
+            }
+
+        }   //GET COORDINATES
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            lbl_selectedFile.Text = null;
+            btn_runScript.Enabled = false;
+
+            // TOOLTIPS
+            ToolTip tt = new ToolTip();
+
+            tt.SetToolTip(this.btn_selectFile, "Select SVG file");
+            tt.SetToolTip(this.btn_runScript, "generate coordinate file");
+        }
+
+        private void btn_runScript_Click(object sender, EventArgs e)
+        {
+            tb_results.Clear();
+            get_coord(globalVars.filePath);
+        }
+
+        private void btn_selectFile_Click(object sender, EventArgs e)
+        {
+            globalVars.filePath = getFilePath();
+
+            int strLength = 50;
+
+            if (globalVars.filePath != null)
+            {
+                if (globalVars.filePath.Length <= strLength)
+                {
+                    lbl_selectedFile.Text = globalVars.filePath;
+                }
+                else
+                {
+                    lbl_selectedFile.Text = "..." + globalVars.filePath.Substring(globalVars.filePath.Length - (strLength - 3));
+                }
+            }
+            else
+            {
+                lbl_selectedFile.Text = null;
+            }
+        }
+
+        private void lbl_selectedFile_TextChanged(object sender, EventArgs e)
+        {
+            // ONLY ENABLE RUN SCRIPT BUTTON IF THERE IS A FILE SELECTED
+            if (lbl_selectedFile.Text.Length > 0 && globalVars.filePath.Length > 0)
+            {
+                btn_runScript.Enabled = true;
+            }
+            else
+            {
+                btn_runScript.Enabled = false;
+            }
+        }
+
+        public static class globalVars
+        {
+            public static string filePath = null;
+        }
+    }
+}
